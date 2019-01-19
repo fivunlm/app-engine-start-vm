9e6500761e12bd318efdfd8cfa2584d3a80ca9b4
Blake West <bwest87@gmail.com>
2019-01-19 10:20:56 -0800

    Updating repo to work with latest version of GCP

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,6 @@
 """`appengine_config` gets loaded when starting a new application instance."""
-import sys
-import os.path
+from google.appengine.ext import vendor
+import os
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
